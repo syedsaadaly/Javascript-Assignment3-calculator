3ef301ec53fccced469de1633f7ce35858809cee
Saad ali <142331196+syedsaadaly@users.noreply.github.com>
2024-03-01 14:19:27 +0500

    Add files via upload

---
+++ JS 3 calculator/app.js
@@ -0,0 +1,19 @@
+var number1 = +prompt("Enter Number 1:");
+var number2 = +prompt("Enter Number 2:");
+
+// let num1=;
+// let num2=;
+
+sum= number1+ number2;
+sub= number1- number2;
+mul= number1 * number2;
+div= number1 / number2;
+reminder= number1 % number2;
+
+console.log('Your given number is: ' + number1+ ' and '  + number2 +"\n");
+console.log('Result \n===================== \n')
+console.log("Sum of two numbers = " + sum);
+console.log("Subtacrtion of two numbers = " + sub);
+console.log("multiplication  of two numbers = " + mul);
+console.log("division  of two numbers = " + div);
+console.log("Reminder  of two numbers = " + reminder);
